d4fc4a1486cc42fa1a78af8f0f8a7d64cb5304c6
Dax Harris <git@dax.gay>
2026-02-21 16:18:50 -0500

    try include

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     };
 
     outputs =
-        { self, nixpkgs, ... }@inputs:
+        { self, nixpkgs, home-manager, ... }@inputs:
         let
             system = "x86_64-linux";
             pkgs = import nixpkgs { inherit system; };
