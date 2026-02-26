947aea836ebde40a7ebe6022593c14f02fca9078
Dax Harris <git@dax.gay>
2026-02-26 10:22:59 -0500

    test fix

--- modules/defaults/default.nix
+++ modules/defaults/default.nix
@@ -5,8 +5,9 @@
         ./system.nix
         ./secrets.nix
         ../../machines/${hostname}/auto.nix
-    ]
-    ++ (lib.optional (builtins.pathExists ../../machines/${hostname}/hardware-configuration.nix) [ ../../machines/${hostname}/hardware-configuration.nix ]);
+        ../../machines/${hostname}/hardware-configuration.nix
+    ];
+    #++ (lib.optional (builtins.pathExists ../../machines/${hostname}/hardware-configuration.nix) [ ../../machines/${hostname}/hardware-configuration.nix ]);
 
     system.stateVersion = "25.11";
 }
