b75ae6e2602ffe8973a8cb6fbf27dd770665604d
Dax Harris <git@dax.gay>
2026-02-26 10:21:31 -0500

    fix paths

--- modules/defaults/default.nix
+++ modules/defaults/default.nix
@@ -4,9 +4,9 @@
         ./boot.nix
         ./system.nix
         ./secrets.nix
-        ./machines/${hostname}/auto.nix
+        ../../machines/${hostname}/auto.nix
     ]
-    ++ (lib.optional (builtins.pathExists ./machines/${hostname}/hardware-configuration.nix) [ ./machines/${hostname}/hardware-configuration.nix ]);
+    ++ (lib.optional (builtins.pathExists ../../machines/${hostname}/hardware-configuration.nix) [ ../../machines/${hostname}/hardware-configuration.nix ]);
 
     system.stateVersion = "25.11";
 }
