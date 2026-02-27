ba2162a69475c3ea2da9798852d6b98b411a2a71
Dax Harris <git@dax.gay>
2026-02-26 21:41:07 -0500

    define a bunch of shell utils

--- modules/defaults/default.nix
+++ modules/defaults/default.nix
@@ -6,9 +6,14 @@
         ./secrets.nix
         ./network.nix
         ./openssh.nix
+        ./shell.nix
         ../../machines/${hostname}/auto.nix
-    ]
-    ++ (lib.optional (builtins.pathExists ../../machines/${hostname}/hardware-configuration.nix) ../../machines/${hostname}/hardware-configuration.nix);
+    ];
 
     system.stateVersion = "25.11";
+    nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+    ];
+    hardware.facter.reportPath = lib.mkIf (builtins.pathExists ../../machines/${hostname}/facter.json) ../../machines/${hostname}/facter.json;
 }
---
+++ modules/defaults/shell.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+{
+    environment.systemPackages = with pkgs; [
+        cargo
+        rustc
+        rustPlatform.bindgenHook
+        pkg-config
+        bashInteractive
+        unar
+        zip
+        xz
+        gzip
+        file-roller
+        wget
+        curl
+        openssl
+        python3
+        pnpm
+        imagemagick
+        ffmpeg-full
+        btop
+        ghostty.terminfo
+        gitui
+        yazi
+        zellij
+        bat
+    ];
+    programs.git = {
+        enable = true;
+    };
+    programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+    };
+    programs.zsh = {
+        enable = true;
+    };
+}
