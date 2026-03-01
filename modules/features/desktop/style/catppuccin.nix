d4573b4c0c1b228d03961153d5446fa4a935436a
Dax Harris <git@dax.gay>
2026-03-01 13:36:49 -0500

    no more catppuccin

--- modules/features/desktop/style/catppuccin.nix
+++ modules/features/desktop/style/catppuccin.nix
@@ -1,12 +1,5 @@
 { pkgs, ... }:
 {
-    catppuccin = {
-        enable = true;
-        flavor = "mocha";
-        accent = "mauve";
-        plymouth.enable = false;
-        tty.enable = false;
-    };
     stylix = {
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
