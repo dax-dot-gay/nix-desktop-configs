836a2847512a220cc8c7d8ab3db27ea39f54e46e
Dax Harris <git@dax.gay>
2026-03-04 14:54:05 -0500

    bind check

--- machines/testbed/users/dax/home.nix
+++ machines/testbed/users/dax/home.nix
@@ -104,6 +104,137 @@
                         };
                     };
                 };
+                ghostty = {
+                    match = [
+                        {app-id = "com.mitchellh.ghostty";}
+                    ];
+                    dynamic.opacity = 0.95;
+                };
+                always-fullscreen = {
+                    match = [
+                        {app-id = "floorp";}
+                        {app-id = "org.mozilla.Thunderbird";}
+                    ];
+                    exclude = [
+                        {title = "Message Filters";}
+                    ];
+                    on-open.open-maximized = true;
+                };
+                chat-windows = {
+                    match = [
+                        {app-id = "equibop";}
+                        {app-id = "cinny";}
+                    ];
+                    on-open = {
+                        open-maximized = true;
+                        open-on-output = "DP-3";
+                    };
+                };
+                password-managers = {
+                    match = [
+                        {app-id = "org.keepassxc.KeePassXC";}
+                    ];
+                    dynamic.block-out-from = "screen-capture";
+                };
+                rounded-corners = {
+                    dynamic = {
+                        geometry-corner-radius = 4;
+                        clip-to-geometry = true;
+                    };
+                };
+            };
+            layer-rules = {
+                awww-daemon = {
+                    match = [
+                        {namespace = "awww-daemon";}
+                    ];
+                    place-within-backdrop = true;
+                };
+            };
+            binds = {
+                "Mod+Return" = {
+                    hotkey-overlay-title = "Open a Terminal";
+                    action.spawn.args = ["ghostty"];
+                };
+                "Mod+D" = {
+                    hotkey-overlay-title = "Open App Launcher";
+                    action.spawn.args = ["vicinae" "toggle"];
+                };
+                "Super+L" = {
+                    hotkey-overlay-title = "Lock the Screen";
+                    action.spawn.args = ["dms" "ipc" "lock" "lock"];
+                };
+                "Mod+N" = {
+                    action.spawn.args = ["nemo"];
+                };
+                XF86AudioRaiseVolume = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1+ -l 1.0";
+                    };
+                };
+                XF86AudioLowerVolume = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.1-";
+                    };
+                };
+                XF86AudioMute = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+                    };
+                };
+                XF86AudioMicMute = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+                    };
+                };
+                XF86AudioPlay = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "playerctl play-pause";
+                    };
+                };
+                XF86AudioStop = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "playerctl stop";
+                    };
+                };
+                XF86AudioPrev = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "playerctl previous";
+                    };
+                };
+                XF86AudioNext = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "playerctl next";
+                    };
+                };
+                XF86MonBrightnessUp = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "brightnessctl --class=backlight set +10%";
+                    };
+                };
+                XF86MonBrightnessDown = {
+                    action.spawn-sh = {
+                        allow-when-locked = true;
+                        command = "brightnessctl --class=backlight set 10%-";
+                    };
+                };
+                "Mod+O" = {
+                    repeat = false;
+                    action.toggle-overview = {};
+                };
+                "Mod+C" = {
+                    repeat = false;
+                    action.close-window = {};
+                };
             };
         };
     };
