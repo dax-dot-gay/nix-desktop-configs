cee4367a99329fbb7a894edf2b0cce37b41f48bd
Dax Harris <git@dax.gay>
2026-03-05 17:20:42 +0000

    disable firewall

--- modules/defaults/network.nix
+++ modules/defaults/network.nix
@@ -3,6 +3,7 @@
     networking = {
         networkmanager.enable = true;
         hostName = hostname;
+        firewall.enable = false;
     };
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
