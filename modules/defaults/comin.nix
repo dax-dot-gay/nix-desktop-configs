1b28ff70d4b81d2b77f0894a0206c0336a83194e
Dax Harris <git@dax.gay>
2026-03-05 08:43:13 -0500

    attempt notif daemon

--- modules/defaults/comin.nix
+++ modules/defaults/comin.nix
@@ -1,6 +1,8 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, comin, ... }:
 {
-    environment.systemPackages = [pkgs.libnotify pkgs.jq pkgs.jq-zsh-plugin];
+    environment.systemPackages = [
+        pkgs.libnotify pkgs.jq pkgs.jq-zsh-plugin pkgs.polkit
+    ];
     services.comin = {
         enable = true;
         deployConfirmer = {
@@ -17,5 +19,37 @@
                 branches.main.name = "main";
             }
         ];
+    };
+    systemd.timers."check-and-notify-update" = {
+        wantedBy = ["timers.target"];
+        timerConfig = {
+            OnBootSec = "60sec";
+            OnUnitActiveSec = "60sec";
+            Unit = "check-and-notify-update.service";
+        };
+    };
+    systemd.services."check-and-notify-update" = {
+        wantedBy = ["default.target"];
+        wants = ["multi-user.target"];
+        script = ''
+            if [ $(echo "$(${comin.packages."x86_64-linux".default} status --json)" | ${pkgs.jq} .deploy_confirmer.submitted) != '""' ]; then
+                for id in $(${pkgs.loginctl} list-sessions -j | ${pkgs.jq} -r '.[] | .session') ; do
+                    if [[ $(${pkgs.loginctl} show-session $id --property=Type) =~ (wayland|x11) ]] ; then
+                        USER=$(${pkgs.loginctl} show-session $id --property=Name --value)
+                        if [ ! -e /home/$USER/.local/share/comin-deployment ] || [ $(cat /home/$USER/.local/share/comin-deployment) != $(echo "$(${comin.packages."x86_64-linux".default} status --json)" | ${pkgs.jq} .deploy_confirmer.submitted) ]; then
+                            echo $(echo "$(${comin.packages."x86_64-linux".default} status --json)" | ${pkgs.jq} .deploy_confirmer.submitted) > /home/$USER/.local/share/comin-deployment
+                            RESPONSE=$(sudo -u $USER DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $USER)/bus notify-send --urgency=critical --app-name="Comin" --action=update=Update "Update Available!" "A deployment is available for deployment. To validate the build, run <sudo journalctl -xeu comin>")
+                            if [ $RESPONSE == "update" ]; then
+                                pkexec comin confirmation accept
+                            fi
+                    fi
+                done
+            fi
+        '';
+        serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+            RemainAfterExit = true;
+        };
     };
 }
