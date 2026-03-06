e8203d130d9b6f551270cc5bae8dcc33313169d5
Dax Harris <git@dax.gay>
2026-03-05 20:01:00 -0500

    set explicit disk ID

--- machines/stryker/configuration.nix
+++ machines/stryker/configuration.nix
@@ -2,7 +2,7 @@
 {
     flake.system-configuration = {
         enable = true;
-        root_disk = "/dev/nvme0n1";
+        root_disk = "nvme-Samsung_SSD_990_EVO_Plus_2TB_S7U6NU0Y756989N";
         users = {
             dax = {
                 superuser = true;
