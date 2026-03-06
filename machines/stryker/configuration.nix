9bd4fbfee2d7a01582cbb3b19f8f8cda9b35b39a
Dax Harris <git@dax.gay>
2026-03-05 22:15:51 -0500

    cleaner mounting

--- machines/stryker/configuration.nix
+++ machines/stryker/configuration.nix
@@ -18,4 +18,26 @@
     flake.secrets.local."data-key" = {
         neededForUsers = true;
     };
+    system.activationScripts = {
+      persist-secret-key = {
+        # Run after /dev has been mounted
+        deps = [ "setupSecrets" ];
+        text =
+          ''
+            if [ ! -d /.persist ]; then
+                install --mode=0700 --owner=root --group=root -d /.persist
+            fi
+
+            cp ${config.sops.secrets."data-key".path} /.persist/data-key
+          '';
+      };
+    }
+    ;
+    environment.etc.crypttab = {
+        mode = "0600";
+        text = ''
+            # <volume-name> <encrypted-device> [key-file] [options]
+            cryptstorage UUID=59ca76bf-c608-47c3-b9ee-7f00b34aa98b /.persist/data-key
+        '';
+    };
 }
