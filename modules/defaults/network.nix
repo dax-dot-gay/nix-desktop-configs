7aea0fd64d056cecc4f0fe1110badde65145ca2e
Dax Harris <git@dax.gay>
2026-03-06 14:14:19 -0500

    disable dhcpcd

--- modules/defaults/network.nix
+++ modules/defaults/network.nix
@@ -2,8 +2,10 @@
 {
     networking = {
         networkmanager.enable = true;
+        networkmanager.dhcp = "internal";
         hostName = hostname;
         firewall.enable = false;
+        dhcpcd.enable = false;
     };
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
