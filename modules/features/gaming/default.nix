7a44b5c833ea2492edbc088d57023f726fae0c5d
Dax Harris <git@dax.gay>
2026-03-06 08:18:35 -0500

    add prism launcher

--- modules/features/gaming/default.nix
+++ modules/features/gaming/default.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
     imports = [
         ./steam.nix
+    ];
+    environment.systemPackages = with pkgs; [
+        (prismlauncher.override {
+            additionalPrograms = [ffmpeg];
+        })
     ];
 }
