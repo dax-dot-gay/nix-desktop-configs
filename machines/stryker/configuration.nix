e87441327e765794d5470b20a4c0ee477ffffb3c
Dax Harris <git@dax.gay>
2026-03-05 20:33:53 -0500

    fix disk name

--- machines/stryker/configuration.nix
+++ machines/stryker/configuration.nix
@@ -2,7 +2,7 @@
 {
     flake.system-configuration = {
         enable = true;
-        root_disk = "nvme-Samsung_SSD_990_EVO_Plus_2TB_S7U6NU0Y756989N";
+        root_disk = "/dev/disk/by-id/nvme-Samsung_SSD_990_EVO_Plus_2TB_S7U6NU0Y756989N";
         users = {
             dax = {
                 superuser = true;
