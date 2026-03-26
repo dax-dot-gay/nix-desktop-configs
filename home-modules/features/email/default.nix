cf864bca1d17ff2f2d88cac00c5f875647b63c6b
Dax Harris <git@dax.gay>
2026-03-26 15:49:55 -0400

    update tray dep

--- home-modules/features/email/default.nix
+++ home-modules/features/email/default.nix
@@ -76,13 +76,12 @@
             Description = "Mozilla Thunderbird System Tray (birdtray) service";
             # I also run xfce4-panel in a user service which provides my
             # system-tray (its service is called shell-panel.service)
-            PartOf = "dms.service";
-            After = "dms.service";
+            PartOf = "graphical-session.target";
          };
          Service = {
             ExecStart = "${pkgs.birdtray}/bin/birdtray";
             Restart = "on-failure";
          };
-         Install.WantedBy = [ "dms.service" ];
+         Install.WantedBy = [ "graphical-session.target" ];
       };
 }
