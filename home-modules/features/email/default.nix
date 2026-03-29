20556a652b08a8c24a0aa9bdc86f971350359920
Dax Harris <git@dax.gay>
2026-03-29 12:46:32 -0400

    add desktop entry

--- home-modules/features/email/default.nix
+++ home-modules/features/email/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
     nixpkgs = {
          overlays = [
@@ -33,44 +33,52 @@
         };
     };
     systemd.user.services.thunderbird-monitor = {
-         Unit = {
-            Description = "Mozilla Thunderbird Monitoring service";
-         };
-         Service = {
-            # I also use connman, so I delay starting Thunderbird-headless until WAN-connectivity is up
-            ExecStart = "${pkgs.thunderbird}/bin/.thunderbird-wrapped_ --headless";
-            Restart = "on-failure";
-         };
-         Install.WantedBy = [ "default.target" ];
+      Unit = {
+         Description = "Mozilla Thunderbird Monitoring service";
       };
-      systemd.user.services.thunderbird-gui = {
-         Unit = {
-            Description = "Mozilla Thunderbird GUI service";
-            Conflicts = "thunderbird-monitor.service";
-            After = "thunderbird-monitor.service";
-         };
-         Service = {
-            Type = "notify";
-            NotifyAccess = "all";
-            ExecStart = let
-               # Best-effort guess when Thunderbird is "ready" (i.e. started up
-               # enough to reject subsequent instance starts), as subsequent
-               # thunderbird invocations shouldn't spawn a new-instance but
-               # can change the active instance (i.e. thunderbird -mail)
-               thunderbird-gui = pkgs.writeShellScriptBin "thunderbird-gui" ''
-                  ${pkgs.thunderbird}/bin/.thunderbird-wrapped_ &
-                  pid="$!"
-                  sleep 3
-                  systemd-notify --ready
-                  wait "$pid"
-               '';
-            in "${thunderbird-gui}/bin/thunderbird-gui";
-            ExecStopPost = "systemctl --user --no-block start thunderbird-monitor";
-         };
+      Service = {
+         # I also use connman, so I delay starting Thunderbird-headless until WAN-connectivity is up
+         ExecStart = "${pkgs.thunderbird}/bin/.thunderbird-wrapped_ --headless";
+         Restart = "on-failure";
       };
+      Install.WantedBy = [ "default.target" ];
+   };
+   systemd.user.services.thunderbird-gui = {
+      Unit = {
+         Description = "Mozilla Thunderbird GUI service";
+         Conflicts = "thunderbird-monitor.service";
+         After = "thunderbird-monitor.service";
+      };
+      Service = {
+         Type = "notify";
+         NotifyAccess = "all";
+         ExecStart = let
+            # Best-effort guess when Thunderbird is "ready" (i.e. started up
+            # enough to reject subsequent instance starts), as subsequent
+            # thunderbird invocations shouldn't spawn a new-instance but
+            # can change the active instance (i.e. thunderbird -mail)
+            thunderbird-gui = pkgs.writeShellScriptBin "thunderbird-gui" ''
+               ${pkgs.thunderbird}/bin/.thunderbird-wrapped_ &
+               pid="$!"
+               sleep 3
+               systemd-notify --ready
+               wait "$pid"
+            '';
+         in "${thunderbird-gui}/bin/thunderbird-gui";
+         ExecStopPost = "systemctl --user --no-block start thunderbird-monitor";
+      };
+   };
 
-      wayland.windowManager.niri.settings.spawn-at-startup = [
-         "sleep 5; ${pkgs.birdtray}/bin/birdtray"
-         "${pkgs.protonmail-bridge-gui}/bin/protonmail-bridge-gui"
-      ];
+   wayland.windowManager.niri.settings.spawn-at-startup = [
+      "sleep 5; ${pkgs.birdtray}/bin/birdtray"
+      "${pkgs.protonmail-bridge-gui}/bin/protonmail-bridge-gui"
+   ];
+
+   xdg.desktopEntries.thunderbird = lib.mkForce {
+      name = "Thunderbird";
+      genericName = "Email Client";
+      exec = "systemctl --user start thunderbird-gui";
+      terminal = false;
+      categories = ["Network" "Feed" "Email"];
+   };
 }
