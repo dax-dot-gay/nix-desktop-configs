6e4ab9cb31a573cf73ec7e64d8696d9d8bc1a7b2
Dax Harris <git@dax.gay>
2026-03-31 12:22:57 -0400

    update tailscale conf

--- modules/features/lesbos-access/default.nix
+++ modules/features/lesbos-access/default.nix
@@ -3,10 +3,17 @@
     hostname,
     ...
 }:
-{ 
-    flake.secrets.global."tailscale/keys/${hostname}" = {};
+{
+    flake.secrets.global."tailscale/keys/${hostname}" = { };
     services.tailscale = {
         enable = true;
         authKeyFile = config.sops.secrets."tailscale/keys/${hostname}".path;
+        extraUpFlags = [ "--advertise-exit-node" ];
     };
+    networking.nftables.enable = true;
+    systemd.services.tailscaled.serviceConfig.Environment = [
+        "TS_DEBUG_FIREWALL_MODE=nftables"
+    ];
+    systemd.network.wait-online.enable = false;
+    boot.initrd.systemd.network.wait-online.enable = false;
 }
