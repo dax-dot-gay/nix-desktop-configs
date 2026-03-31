ce427b3b38edcabfac2197480e7635e41836b9dc
Dax Harris <git@dax.gay>
2026-03-31 12:28:45 -0400

    add ssh config

--- home-modules/features/lesbos-access/default.nix
+++ home-modules/features/lesbos-access/default.nix
@@ -1,4 +1,11 @@
-{ ... }:
+{ config, ... }:
 {
     services.trayscale.enable = true;
+    programs.ssh = {
+        enable = true;
+        matchBlocks.lesbos = {
+            host = "192.168.30.*";
+            identityFile = "/home/${config.homeflake.info.username}/.ssh/id_ed25519.lesbos";
+        };
+    };
 }
