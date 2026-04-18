2c7240827e76722684d405b02301edede05acc64
Dax Harris <git@dax.gay>
2026-04-18 09:13:26 -0400

    fix tailscale

--- modules/features/lesbos-access/default.nix
+++ modules/features/lesbos-access/default.nix
@@ -1,14 +1,9 @@
 {
-    config,
-    hostname,
     ...
 }:
 {
-    flake.secrets.global."tailscale/keys/${hostname}" = { };
     services.tailscale = {
         enable = true;
-        authKeyFile = config.sops.secrets."tailscale/keys/${hostname}".path;
-        extraUpFlags = [ "--advertise-exit-node" ];
     };
     networking.nftables.enable = true;
     systemd.services.tailscaled.serviceConfig.Environment = [
