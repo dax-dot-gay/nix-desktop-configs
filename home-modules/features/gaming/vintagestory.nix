c5ce9cb59a0cf0e9446f83d84567bea69fd4c7a5
Dax Harris <git@dax.gay>
2026-05-09 11:44:54 -0400

    add vs versions

--- home-modules/features/gaming/vintagestory.nix
+++ home-modules/features/gaming/vintagestory.nix
@@ -4,11 +4,13 @@
     home.packages = with pkgs; [
         vintagestoryPackages.rustique
         vintagestoryPackages.vs-launcher
+        vintagestoryPackages.latest
     ];
     programs.vs-launcher = {
         enable = true;
         settings.gameVersions = [
-            pkgs.vintagestoryPackages.latest
+            pkgs.vintagestoryPackages.v1-22-2
+            pkgs.vintagestoryPackages.v1-22-0
         ];
     };
 }
